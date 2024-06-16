4fa9db16ec3c9e89efb73bbfa84ba6324d2d346c
hoo <tbnsok40@gmail.com>
2024-06-16 16:00:13 +0900

    settings

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+// next 설정 파일임
+export default nextConfig;
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '@/styles/globals.css';
+
+// app component
+export default function App({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
+
+// 채팅 -> 서버 -> 클라-넥스트
