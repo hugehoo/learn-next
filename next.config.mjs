1c62290b6246049457c048218b2cfa602beee00d
hoo <tbnsok40@gmail.com>
2024-06-23 10:00:04 +0900

    section5

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        port: '',
+        pathname: '/user/**'
+
+      }
+    ]
+  }
 };
 // next 설정 파일임
 export default nextConfig;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ProductList from '@/components/ProductList';
 
 // `/` 에 해당하는 페이지 컴포넌트
-function HomePage() {
-	return <div>HELLO NEXT.JS</div>;
+function ProductPage() {
+	return (
+		<div>
+			<h1>상품목록 페이지</h1>
+			<ProductList />
+		</div>
+	);
 }
 
 /**
@@ -11,4 +18,12 @@ function HomePage() {
  * 3. 장바구니 페이지 - `/cart`
  */
 
-export default HomePage;
+export default ProductPage;
+
+/**
+ * 첨엔 next.js 의 기능이 아닌 기본 리액트 기능을 활용해 rest api 를 구현해본다.
+ * 그 후에 next.js 의 rest api 를 사용해보자.
+ */
+
+// 3 주면 스위프트 문법 개넉넉하게 뗄 수 있다.
+// 스위프트 문법 다 떼면, 이 인강도 괜찮고
